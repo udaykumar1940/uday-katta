e7aed552b69557ad08d72b3ef07b910906ee431a
udaykumar1940 <udaykumar1940@gmail.com>
2018-11-11 16:08:37 +0530

    asdsd

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/posts/:id', async(req,res)=>{
-    var author  = req.params.id
+app.get('/posts', auth.checkAuthenticated, async(req,res)=>{
+    var author  = req.userId; 
     var posts = await postobj.find({author})
     res.send(posts);
 });
@@ -32,48 +32,52 @@ app.post('/post', auth.checkAuthenticated, (req,res)=>{
 
     post.save((err,result)=>{
         if(err){
-            console.error('Post Saving error...');
             res.send({message:"saving post error..."})
         }
     })
-    //console.log(userData.email);
     res.send({message:"Added Successfully"});
 })
-
 
 // app.post('/register', auth.register)
 
 // app.post('/login', auth.login)
 
-app.get('/users', async (req,res)=>{
+app.get('/users', auth.checkAuthenticated,async (req,res)=>{
     try {
         var users = await userobj.find({},'-password -__v') // To exclude password and version(__v) from the object
         res.send(users)
     } catch (error) {
-        console.error(error)
         res.sendStatus(500)
     }
 })
 
-app.get('/profile/:id', async (req,res)=>{
+app.get('/profile/:id', auth.checkAuthenticated, async (req,res)=>{
     try{
         var user = await userobj.findById(req.params.id, '-__v')
         res.send(user)
     }catch(error){
-        console.error(error)
         res.sendStatus(500)
     }
 })
 
 app.delete('/user/delete/:id', auth.checkAuthenticated, async(req,res)=>{
-     console.log("deleting a user")
      var _id = req.params.id;
      if(_id!='id'){
-        console.log("2 -->"+_id)
         await userobj.deleteOne({_id})
         res.send({message:"deleted Successfully... "});
     }else{
          res.send({message:"delete fail... "});
+    }
+   
+});
+
+app.delete('/post/delete/:id', auth.checkAuthenticated, async(req,res)=>{
+     var _id = req.params.id;
+     if(_id!='id'){
+        await postobj.deleteOne({_id})
+        res.send({message:"Post deleted Successfully... "});
+    }else{
+         res.send({message:"Post delete failed... "});
     }
    
 });
@@ -91,13 +95,107 @@ app.get('/sessionUser', auth.checkAuthenticated, async(req,res)=>{
 
 });
 
+function A(ids){
+    var content = '';
+
+    ids.forEach( async element  => {
+        //console.log("element:"+element);
+        userList2 = await userobj.find({email:element});
+        console.log("=>"+userList2[0]);
+        homeList = await homeobj.find({author:userList2[0]._id});
+       // console.log("----------->"+homeList);
+
+        await homeList.forEach( element2=>{
+            //console.log("--->"+element2.description);
+            console.log("content{sdsdsd}:"+content)
+            content+=element2.description;
+        })
+    })
+      console.log("finished");
+    
+}
+
+const sendMail = async (ids,toEmailId) => {
+ 
+    var content = '';
+
+    for(var i=0;i<ids.length;i++){
+        userList2 = await userobj.find({email:ids[i]});
+            console.log("=>"+userList2[0]);
+            homeList = await homeobj.find({author:userList2[0]._id});
+        for(var j=0;j<homeList.length;j++){
+        content+= "\n\n"+homeList[j].description;
+        }
+
+        if(i==(ids.length-1)){
+            await transporter.sendMail({
+                from: 'fake33396@gmail.com',
+                to: toEmailId,
+                subject: 'Monthly Reports',
+                text: await content
+            }, function(error, info){
+                if (error) {
+                    console.log("error here:"+error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            })
+        }
+    }
+
+}
+
+    
+app.post('/sendMail', auth.checkAuthenticated, async (req,res)=>{
+
+    var author = req.userId; 
+    //var author_id: mongoose.ObjectId(author);
+    console.log(author);
+    var userList = await userobj.find({_id:author});
+    var toEmailId;
+    var homeList;
+    var userList2 ;
+    console.log(userList);
+    if(userList!=null){
+        toEmailId = userList[0].email;
+    }
+    var postData = req.body.toString();
+    var ids = postData.split(",");
+
+
+sendMail(ids,toEmailId);
+
+
+/*    ids.forEach(element => {
+    
+    var mailOptions = {
+        from: 'fake33396@gmail.com',
+        to: element,
+        subject: 'Hi '+element+'!!! Sending Email using Node.js',
+        text: 'Angular js Test mail. pelase Ignore it!!'
+    };
+
+     transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log("error here:"+error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    });
+  */
+
+    res.send({message:"Sent Successfully"});
+})
+
+
 // mongoose 5.x no longer required the useMongoClient:true
  mongoose.connect('mongodb://root:udaymongo1@ds125618.mlab.com:25618/udaymongo', {useNewUrlParser:true}, (err)=>{
     if(!err){
         console.log('Mongo DB connected to MLab');
     }
 })
-
 
 /*
 mongoose.connect('mongodb://localhost:27017/admin', {useNewUrlParser:true}, (err)=>{
@@ -107,36 +205,23 @@ mongoose.connect('mongodb://localhost:27017/admin', {useNewUrlParser:true}, (err
 
 })
 */
-/*
+
 var smtpConfig = {
     host: 'smtp.gmail.com',
     post:587,
     secure: false, // use SSL, 
                   // you can try with TLS, but port is then 587
     auth: {
-      user: 'udaykumar1940@gmail.com',
-        pass: 'Jesusmindtree531'
+      user: 'fake33396@gmail.com',
+        pass: 'fakefake531'
     }
   };
 
+var mailOptions = {};
+
 var transporter = nodemailer.createTransport(smtpConfig);
 
-    var mailOptions = {
-    from: 'udaykumar1940@gmail.com',
-    to: 'udaykumar1940@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log("error here:"+error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-    });
-*/
 
 app.use('/auth',auth.router);
 app.use('/home',home)
-app.listen( process.env.PORT | 3000);
+app.listen(process.env.PORT || 3000);
